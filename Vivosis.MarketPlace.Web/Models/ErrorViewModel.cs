66c7c792a1ff94694cff6508c9fccb3ac045b21a
Muhammed Çelik <celikmuhammed21@hotmail.com>
2020-11-27 16:04:42 +0300

    Islemlerin bir arayuz uzerinden yapilabilmesi icin web projesi eklendi.

---
+++ Vivosis.MarketPlace.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vivosis.MarketPlace.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
