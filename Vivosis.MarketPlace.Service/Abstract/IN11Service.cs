a69f87a47b8786786580116ae143d81be096025e
Muhammed Çelik <celikmuhammed21@hotmail.com>
2020-11-30 13:07:37 +0300

    N11 servisi eklendi.

---
+++ Vivosis.MarketPlace.Service/Abstract/IN11Service.cs
@@ -0,0 +1,13 @@
+﻿using N11CategoryService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vivosis.MarketPlace.Service.Abstract
+{
+    public interface IN11Service
+    {
+        GetTopLevelCategoriesResponse1 Test();
+        GetSubCategoriesResponse1 Test2();
+    }
+}
---
+++ Vivosis.MarketPlace.Service/Concrete/N11Service.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vivosis.MarketPlace.Service.Abstract;
+using N11CategoryService;
+using Microsoft.EntityFrameworkCore;
+using Vivosis.MarketPlace.Data.Entities;
+using Vivosis.MarketPlace.Data;
+using System.Linq;
+
+namespace Vivosis.MarketPlace.Service.Concrete
+{
+    public class N11Service :IN11Service
+    {
+        Store _store;
+        Authentication _auth;
+        public N11Service(MarketPlaceDbContext dbContex)
+        {
+            var store = dbContex.Stores.Where(store => store.name.ToLower().Equals("n11")) ?? throw new InvalidOperationException("N11 sisteminizde kayitli degil.");
+            _store = store.Include(s=>s.StoreProducts).Include(s=>s.StoreCategories).First();
+            _auth = new Authentication
+            {
+                appKey = _store.api_key,
+                appSecret = _store.secret_key
+            };
+        }
+        public GetTopLevelCategoriesResponse1 Test()
+        {
+            CategoryServicePortClient c = new CategoryServicePortClient();
+            GetTopLevelCategoriesRequest req = new GetTopLevelCategoriesRequest();
+            req.auth = _auth;
+            GetTopLevelCategoriesResponse1 res = c.GetTopLevelCategoriesAsync(req).Result;
+            return res;
+        }
+        public GetSubCategoriesResponse1 Test2()
+        {
+            CategoryServicePortClient c = new CategoryServicePortClient();
+            var req = new GetCategoryAttributesRequest();
+            req.auth = _auth;
+            req.categoryId = 1002841;
+            var res = c.GetCategoryAttributesAsync(req).Result;
+            return new GetSubCategoriesResponse1();
+        }
+    }
+}
