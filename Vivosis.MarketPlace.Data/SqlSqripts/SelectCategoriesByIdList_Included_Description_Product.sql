1a9dc02d01a0f0e41b7f38f1c1accfc3583c312f
Muhammed Çelik <celikmuhammed21@hotmail.com>
2020-11-30 13:03:07 +0300

    Adonet categori getirme scriptleri yazildi.

---
+++ Vivosis.MarketPlace.Data/SqlSqripts/SelectCategoriesByIdList_Included_Description_Product.sql
@@ -0,0 +1,5 @@
+﻿SELECT * FROM ofsimo_opencart.category as c 
+left join category_description as d on d.category_id = c.category_id
+left join product_to_category as pc on pc.category_id = c.category_id
+left join product as p on p.product_id = pc.product_id
+where c.category_id in ({0});
---
+++ Vivosis.MarketPlace.Data/SqlSqripts/SelectCategories_Included_Description_Product.sql
@@ -0,0 +1,4 @@
+﻿SELECT * FROM ofsimo_opencart.category as c 
+left join category_description as d on d.category_id = c.category_id
+left join product_to_category as pc on pc.category_id = c.category_id
+left join product as p on p.product_id = pc.product_id
