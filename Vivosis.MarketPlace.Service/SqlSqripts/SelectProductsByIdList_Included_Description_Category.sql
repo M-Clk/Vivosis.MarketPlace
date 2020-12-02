50fd1f6403e0534113c9157daa0f2c5df7ba35a8
Muhammed Çelik <celikmuhammed21@hotmail.com>
2020-12-02 14:25:55 +0300

    Script dosyalarinda spesifik belirtilen veritabani silinerek dinamik veritabanini destekler hale getirildi.

--- Vivosis.MarketPlace.Service/SqlSqripts/SelectProductsByIdList_Included_Description_Category.sql
+++ Vivosis.MarketPlace.Service/SqlSqripts/SelectProductsByIdList_Included_Description_Category.sql
@@ -1,4 +1,4 @@
-﻿SELECT * FROM ofsimo_opencart.product as p 
+﻿SELECT * FROM product as p 
 left join product_description as d on d.product_id = p.product_id
 left join product_to_category as pc on pc.product_id = p.product_id
 left join category as c on c.category_id = pc.category_id
