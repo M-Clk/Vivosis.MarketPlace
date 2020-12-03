190dfe16c89b2e056dbc88fd257a57547d4b04cc
Muhammed Çelik <celikmuhammed21@hotmail.com>
2020-12-03 13:33:06 +0300

    ProductsController urunleri getirecek sekilde guncellendi. Ayrica view eklendi.

--- Vivosis.MarketPlace.Web/Controllers/ProductsController.cs
+++ Vivosis.MarketPlace.Web/Controllers/ProductsController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Vivosis.MarketPlace.Data;
-using Vivosis.MarketPlace.Data.Entities;
 using Vivosis.MarketPlace.Service.Abstract;
 
 namespace Vivosis.MarketPlace.Web.Controllers
@@ -20,56 +17,27 @@ namespace Vivosis.MarketPlace.Web.Controllers
             _context = context;
             _globalService = globalService;
         }
-        // GET: api/Products
+        // GET: Products
         public IActionResult Index()
         {
-            var products = _globalService.GetProducts();
-            return Ok(products);
+            if(ModelState.IsValid)
+            {
+                var products = _globalService.GetProducts();
+                return View(products);
+            }
+            return View();
         }
         [HttpPost]
-        public IActionResult Index(IEnumerable<int> idList)
+        public IActionResult Selecteds(IEnumerable<int> idList)
         {
             var products = _globalService.GetProducts(idList);
             return Ok(products);
         }
-        // GET: api/Products/5
-        [HttpGet("Products/{id}")]
-        public async Task<IActionResult> GetProduct(int id)
+        // GET: Products/5
+        public IActionResult Get(int id)
         {
             var product = _globalService.GetProducts(new List<int> { id });
-
-            if(product == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(product);
-        }
-        // GET: api/Categories
-        [HttpGet("Categories")]
-        public IActionResult GetCategory()
-        {
-            var products = _globalService.GetCategories();
-            return Ok(products);
-        }
-        [HttpPatch("Categories")]
-        public IActionResult GetCategory(IEnumerable<int> idList)
-        {
-            var products = _globalService.GetCategories(idList);
-            return Ok(products);
-        }
-        // GET: api/Categories/5
-        [HttpGet("Categories/{id}")]
-        public async Task<IActionResult> GetCategory(int id)
-        {
-            var product = _globalService.GetCategories(new List<int> { id });
-
-            if(product == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(product);
+            return View(product);
         }
     }
 }
