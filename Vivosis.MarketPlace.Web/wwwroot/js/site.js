5d02b2b63946fe3da466fbc863848246ee673835
Muhammed Çelik <celikmuhammed21@hotmail.com>
2020-12-04 17:07:54 +0300

    Account/Add sayfasi bolundu. AddAdmin ve AdCustomer seklinde iki sayfa halinde yazildi. AddAdmin sayfasinda db ile ilgili bilgiler istenmez. Ayrica random sifre uretme fonksiyonu site.js dosyasina yazildi.

--- Vivosis.MarketPlace.Web/wwwroot/js/site.js
+++ Vivosis.MarketPlace.Web/wwwroot/js/site.js
@@ -1,4 +1,11 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
+function generateRandomPass(elementId) {
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    var charactersLength = characters.length;
+    for (var i = 0; i < 15; i++)
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    document.getElementById(elementId).value = result;
+}
 // Write your JavaScript code.
