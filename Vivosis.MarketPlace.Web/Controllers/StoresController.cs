c1184d54225719b36455c30637e09c7d072bda01
Muhammed Çelik <celikmuhammed21@hotmail.com>
2020-12-07 09:25:51 +0300

    StoresController eklendi. Index.cshtml sayfasi eklendi.

---
+++ Vivosis.MarketPlace.Web/Controllers/StoresController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Vivosis.MarketPlace.Data.Entities;
+using Vivosis.MarketPlace.Service.Abstract;
+
+namespace Vivosis.MarketPlace.Web.Controllers
+{
+    public class StoresController :Controller
+    {
+        IStoreService _storeService;
+        public StoresController(IStoreService storeService)
+        {
+            _storeService = storeService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index([FromForm] StoreUser storeUser)
+        {
+            if(ModelState.IsValid)
+            {
+                if(_storeService.AddStore(storeUser))
+                    return View();
+            }
+            return View(storeUser);
+        }
+    }
+}
