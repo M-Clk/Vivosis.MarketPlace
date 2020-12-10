3bf09acf926f331c95a971ded00af8f66f23079e
Muhammed Çelik <celikmuhammed21@hotmail.com>
2020-12-10 10:48:25 +0300

    SyncOptions Metodu Yazildi Uzak veritabanindaki options ve product options verilerini getirip yerel veritabanina yoksa ekleyen varsa guncelleyen SyncOptions metodu yazildi.

--- Vivosis.MarketPlace.Service/Concrete/CommonService.cs
+++ Vivosis.MarketPlace.Service/Concrete/CommonService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MySql.Data.MySqlClient;
 using System;
@@ -13,11 +14,115 @@ namespace Vivosis.MarketPlace.Service.Concrete
     {
         MarketPlaceDbContext _dbContext;
         MySqlConnection _connection;
-        public CommonService(MarketPlaceDbContext dbContext, IHttpContextAccessor httpContextAccessor)
+        public CommonService(MarketPlaceDbContext dbContext, IHttpContextAccessor httpContextAccessor, UserManager<SystemUser> userManager)
         {
             _dbContext = dbContext;
-            _connection = new MySqlConnection(httpContextAccessor.HttpContext.Request.Cookies["VivosisConnectionString"]);
+            var user = userManager.FindByNameAsync(httpContextAccessor.HttpContext.User.Identity.Name).Result;
+            var connectionString = $"Server={user.Server}; Database={user.DbName}; Uid={user.DbUserName}; Pwd={user.DbPassword};";
+            _connection = new MySqlConnection(connectionString);
         }
+
+        public void SyncLocalOptions()
+        {
+            _connection.Open();
+            var command = _connection.CreateCommand();
+            command.LoadScript("SelectOptions_Included_OptionValue_ProductOption_ProductOptionValue");
+            var dataReader = command.ExecuteReader();
+            while(dataReader.Read())
+            {
+                var optionId = (int)dataReader["option_id"];
+                var option = _dbContext.Options.FirstOrDefault(o => o.option_id == optionId);
+                var isOptionExist = option != null;
+                if(!isOptionExist)
+                {
+                    option = new Option();
+                    option.option_id = optionId;
+                    option.type = (string)dataReader["option_type"];
+                    option.name = (string)dataReader["option_name"];
+                    option.sort_order = (int)dataReader["option_sort_order"];
+                }
+                if(dataReader["product_option_id"] != DBNull.Value)
+                {
+                    var productOption = _dbContext.ProductOptions.FirstOrDefault(po => po.product_option_id == (int)dataReader["product_option_id"]);
+                    if(productOption != null)
+                    {
+                        productOption.is_required = (bool)dataReader["product_option_required"];
+                        productOption.value = (string)dataReader["product_option_value"];
+                        _dbContext.ProductOptions.Update(productOption);
+                    }
+                    else
+                    {
+                        productOption = new ProductOption
+                        {
+                            product_option_id = (int)dataReader["product_option_id"],
+                            option_id = optionId,
+                            product_id = (int)dataReader["product_option_product_id"],
+                            is_required = (bool)dataReader["product_option_required"],
+                            value = (string)dataReader["product_option_value"]
+                        };
+                        _dbContext.ProductOptions.Add(productOption);
+                    }
+                }
+                if(dataReader["option_value_id"] != DBNull.Value)
+                {
+                    var optionValue = _dbContext.OptionValues.FirstOrDefault(ov => ov.option_value_id == (int)dataReader["option_value_id"]);
+                    if(optionValue != null)
+                    {
+                        optionValue.name = (string)dataReader["option_value_name"];
+                        optionValue.sort_order = (int)dataReader["option_value_sort_order"];
+                        _dbContext.OptionValues.Update(optionValue);
+                    }
+                    else
+                    {
+                        optionValue = new OptionValue
+                        {
+                            option_value_id = (int)dataReader["option_value_id"],
+                            option_id = optionId,
+                            name = (string)dataReader["option_value_name"],
+                            sort_order = (int)dataReader["option_value_sort_order"]
+                        };
+                        _dbContext.OptionValues.Add(optionValue);
+                    }
+                }
+                if(dataReader["product_option_value_id"] != DBNull.Value)
+                {
+                    var productOptionValue = _dbContext.ProductOptionValues.FirstOrDefault(po => po.product_option_value_id == (int)dataReader["product_option_value_id"]);
+                    if(productOptionValue != null)
+                    {
+                        productOptionValue.option_value_id = (int)dataReader["option_value_id"];
+                        productOptionValue.product_option_id = (int)dataReader["product_option_id"];
+                        productOptionValue.quantity = (int)dataReader["product_option_value_quantity"];
+                        productOptionValue.point = (int)dataReader["product_option_value_points"];
+                        productOptionValue.price = (decimal)dataReader["product_option_value_price"];
+                        productOptionValue.weight = (decimal)dataReader["product_option_value_weight"];
+                        _dbContext.ProductOptionValues.Update(productOptionValue);
+                    }
+                    else
+                    {
+                        productOptionValue = new ProductOptionValue()
+                        {
+                            product_option_value_id = (int)dataReader["product_option_value_id"],
+                            option_value_id = (int)dataReader["option_value_id"],
+                            product_option_id = (int)dataReader["product_option_id"],
+                            quantity = (int)dataReader["product_option_value_quantity"],
+                            point = int.Parse((string)dataReader["product_option_value_points"]),
+                            price = decimal.Parse((string)dataReader["product_option_value_price"]),
+                            weight = decimal.Parse((string)dataReader["product_option_value_weight"])
+                        };
+                        _dbContext.ProductOptionValues.Add(productOptionValue);
+                    }
+                }
+                if(isOptionExist)
+                    _dbContext.Options.Update(option);
+                else
+                    _dbContext.Options.Add(option);
+                _dbContext.SaveChanges();
+            }
+            _connection.Close();
+            command.Dispose();
+            dataReader.Dispose();
+        }
+
         public void SyncLocalProducts()
         {
             _connection.Open();
