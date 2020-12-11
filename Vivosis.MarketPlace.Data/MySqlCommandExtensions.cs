c2a039807431c253200e51ff4af4391411c5c48a
Muhammed Çelik <celikmuhammed21@hotmail.com>
2020-12-11 16:48:43 +0300

    MySql Scripts Lokasyonu Degistirildi MySql Scriptleri Service katmanindan Data katmanina aktarildi. Icinde LoadScript metodu bulunan MySqlCommandExtensions sinifi da ayni sekilde Service katmanindan Data katmanina aktarildi.

--- Vivosis.MarketPlace.Data/MySqlCommandExtensions.cs
+++ Vivosis.MarketPlace.Data/MySqlCommandExtensions.cs
@@ -2,13 +2,13 @@
 using System;
 using System.IO;
 
-namespace Vivosis.MarketPlace.Service
+namespace Vivosis.MarketPlace.Data
 {
     public static class MySqlCommandExtensions
     {
         public static void LoadScript(this MySqlCommand command, string scriptFileName, params string[] arguments)
         {
-            var sqlScriptPath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).FullName, "Vivosis.MarketPlace.Service", "SqlSqripts", $"{scriptFileName}.sql");
+            var sqlScriptPath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).FullName, "Vivosis.MarketPlace.Data", "SqlSqripts", $"{scriptFileName}.sql");
             var script = File.ReadAllText(sqlScriptPath);
             if(arguments.Length > 0)
                 script = string.Format(script, arguments);
