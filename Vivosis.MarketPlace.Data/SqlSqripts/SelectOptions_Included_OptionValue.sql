618ee3e191b9cabffa138328cbd82a4530034b26
Muhammed Çelik <celikmuhammed21@hotmail.com>
2020-12-12 23:49:52 +0300

    SelectOptions Dosyasi Ikiye Bolundu SelectOptions sorgusu yapan dosya Option-OptionValue ve ProductOption-ProductOptionValue sorgularini ayri ayri yapacak sekilde ikiye bolundu.

---
+++ Vivosis.MarketPlace.Data/SqlSqripts/SelectOptions_Included_OptionValue.sql
@@ -0,0 +1,12 @@
+﻿SELECT 
+o.option_id as 'option_id', 
+o.type  as 'option_type', 
+o.sort_order  as 'option_sort_order', 
+od.name  as 'option_name', 
+ov.option_value_id  as 'option_value_id', 
+ov.sort_order as 'option_value_sort_order',
+ovd.name as 'option_value_name'
+FROM `option` as o
+Left join option_description as od on od.option_id = o.option_id
+Left join option_value as ov on ov.option_id = o.option_id
+left join option_value_description as ovd on ovd.option_id = o.option_id
---
+++ Vivosis.MarketPlace.Data/SqlSqripts/SelectProductOptions_Included_ProductOptionValue.sql
@@ -0,0 +1,14 @@
+﻿SELECT 
+po.product_option_id as 'product_option_id',
+po.required as 'product_option_required',
+po.value as 'product_option_value',
+po.product_id as 'product_option_product_id',
+po.option_id as 'product_option_option_id',
+pov.product_option_value_id as 'product_option_value_id',
+pov.option_value_id as 'option_value_id',
+pov.quantity as 'product_option_value_quantity',
+CONCAT(pov.price_prefix, pov.price) as 'product_option_value_price',
+CONCAT(pov.points_prefix, pov.points) as 'product_option_value_points',
+CONCAT(pov.weight_prefix, pov.weight) as 'product_option_value_weight'
+FROM product_option as po
+left join product_option_value as pov on pov.product_option_id = po.product_option_id
