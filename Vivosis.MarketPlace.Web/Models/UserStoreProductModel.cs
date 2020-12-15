4ca39690ab828a550fb1f59a798783dab8ba2e57
Muhammed Çelik <celikmuhammed21@hotmail.com>
2020-12-16 00:11:24 +0300

    UserStoreProductModel Eklendi Musterinin product ve storeproductlarini birlikte almasi icin UserStoreProductModel eklendi.

---
+++ Vivosis.MarketPlace.Web/Models/UserStoreProductModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vivosis.MarketPlace.Data.Entities;
+
+namespace Vivosis.MarketPlace.Web.Models
+{
+    public class UserStoreProductModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<StoreUser> Stores { get; set; }
+    }
+}
