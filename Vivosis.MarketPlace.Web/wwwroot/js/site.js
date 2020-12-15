5e69802e3fe1bdc3b599d2a61eed3c69fb3e95ba
Muhammed Çelik <celikmuhammed21@hotmail.com>
2020-12-15 22:21:53 +0300

    site.js Yeni Fonksiyonlar Eklendi showPopup fonksiyonu calisacak sekilde guncellendi. Ayrica Datatable konfigurasyonunu yapan fonksiyon Datatable kullanilan herhangi bir sayfada kullanilabilecek sekilde site.js dosyasina yazildi.

--- Vivosis.MarketPlace.Web/wwwroot/js/site.js
+++ Vivosis.MarketPlace.Web/wwwroot/js/site.js
@@ -8,16 +8,30 @@ function generateRandomPass(elementId) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     document.getElementById(elementId).value = result;
 }
-// Write your JavaScript code.
-showPopup = (url, title) => {
-    $.ajax(
+function showPopup(url, title) {
+    jQuery.ajax(
         {
             type: "GET",
-            url = url,
+            url: url,
             success: function (res) {
-                $("#form-modal .modal-body").html(res);
-                $("#form-modal .modal-title").html(title);
-                $("#form-modal").modal("show");
+                jQuery("#form-modal .modal-body").html(res);
+                jQuery("#form-modal .modal-title").html(title);
+                jQuery("#form-modal").modal("show");
             }
-        });
+        }).then(
+            function () {
+                jQuery("#option").children().first().children().addClass("active");
+                jQuery("#option-content").children().first().addClass("active");
+                jQuery("input[data-size='xs']").bootstrapToggle();
+            });
 }
+$('#mytable').DataTable(
+    {
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.10.22/i18n/Turkish.json'
+        },
+        columnDefs: [
+            { orderable: false, targets: 0 }
+        ],
+        order: [[1, 'asc']]
+    });
